7fbb6176a4c3217456771c9304cfdbdd45b0986c
Thomas Seeley <seeleypersonal@gmail.com>
2026-05-04 07:27:16 -0400

    trust wheel users on the nix daemon

--- modules/nixos/base.nix
+++ modules/nixos/base.nix
@@ -1,5 +1,6 @@
 { config, pkgs, ... }: {
   nix.settings.experimental-features = [ "nix-command" "flakes" ];
+  nix.settings.trusted-users = [ "root" "@wheel" ];
   nix.gc = {
     automatic = true;
     dates = "weekly";
